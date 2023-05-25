a6a648a80728c948e21d95a70102c1c728c231cc
SSSanga <ksa901125@gmail.com>
2023-05-25 18:26:16 +0900

    pollconfusion질문과 보기는 나타나게 했고 정답도 array진행. 정답만 어떻게?

---
+++ src/May25thPollpractice.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class May25thPollpractice {
+    public static void main(String[] args) {
+        System.out.print
+
+        // return 0 ;
+
+    }
+}
--- src/pollconfusion.java
+++ src/pollconfusion.java
@@ -7,27 +7,27 @@ public class pollconfusion {
         Scanner myObj = new Scanner(System.in);
             
         String[] quests = { "1. 교수는 수업 전 강의 목표를 명확히 제시하였습니까?",
-                "2. 강의의 내용은 체계적이고 성의깄게 구성되었는가?",
+                "2. 강의의 내용은 체계적이고 성의있게 구성되었는가?",
                 "3. 교수는 강의 내용에 대해 전문적 지식이 있었는가?",
                 "4. 강의 진행 속도는 적절하였는가?" };
-        String[] total = {"",""};
-        // String[] answers = { " ", "(1)", "(2)", "(3)", "(4)" };
+        String[] answers = { " ", "(1)", "(2)", "(3)", "(4)" };
+        // String [] total = {};
 
         for (int qnumber = 0; qnumber < quests.length; qnumber = qnumber + 1) {
             System.out.println(quests[qnumber]);
             System.out.println("(1) 전혀 아니다." + "(2) 아니다." + "(3) 그렇다." + "(4) 매우 그렇다.");
+            
             int count = 0;
-            total[count] = myObj.nextLine();
-            count = count + 1;
+            answers[count] = myObj.next();
+            // count = count + 1;
             // System.out.println (answers[count]);
             }
-            for (int reply = 0; reply < total.length; reply = reply +1){
-                System.out.print (total[reply] + "/");
+            // for (int reply = 0; reply < total.length; reply = reply +1){
+            //     System.out.print (total[reply] + "/");
             }
-            System.out.println();
-
+                // System.out.println();
+                // return 0;
         }
-        // return 0;
-    }
+
 
 

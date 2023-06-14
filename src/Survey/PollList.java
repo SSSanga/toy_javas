d30704679f5e1724e940cde0a397aa2fabafb65e
SSSanga <ksa901125@gmail.com>
2023-06-14 17:50:00 +0900

    PollInput으로 scanner기능 넣어주고 질문은 어떻게 내놓지?

--- src/Survey/PollList.java
+++ src/Survey/PollList.java
@@ -1,8 +1,14 @@
 package Survey;
 
 import java.sql.*;
+import java.util.Scanner;
+
 
 public class PollList {
+    public PollList ()
+    {    } // PollList 초기화
+
+    
     // 문항 초기화(), 입력()을 하기.
     // 문항 초기화가 있다는 것은 질문 인스턴스화가 필요하다는건가??왜??
     public static void main(String[] args) {
@@ -15,22 +21,29 @@ public class PollList {
             System.out.println("DB연결OK");
 
             Statement statement = connection.createStatement();
-            String survey = "SELECT * FROM survey";
-            ResultSet resultSet = statement.executeQuery(survey);
-            // while (resultSet.next()) {
-            // System.out.println(resultSet.getString("SURVEY"));
+            
+
+            // String survey = "SELECT * FROM survey";
+            // ResultSet resultSet = statement.executeQuery(survey);
+            // // while (resultSet.next()) {
+            // // System.out.println(resultSet.getString("SURVEY"));
+            // // }
+            // String surveyID = "'SURVEY_02'";
+            // survey = "SELECT SURVEY" + "FROM survey" + "WHERE SURVEY_ID = '" + surveyID + "'";
+            // // if (resultSet.next()) 이렇게 해도 02에 대한 내용이 나오지 않음.
+            // // if는 그냥 true여서 한줄 나오고 끝남.
+            // // while은 false가 될때까지 반복함.
+            // {
+            //     System.out.println(resultSet.getString("SURVEY"));
             // }
-            String surveyID = "'SURVEY_02'";
-            survey = "SELECT SURVEY" + "FROM survey" + "WHERE SURVEY_ID = '" + surveyID + "'";
-            // if (resultSet.next()) 이렇게 해도 02에 대한 내용이 나오지 않음.
-            // if는 그냥 true여서 한줄 나오고 끝남.
-            // while은 false가 될때까지 반복함.
-            {
-                System.out.println(resultSet.getString("SURVEY"));
-            }
         } catch (Exception e) {
             // TODO: handle exception
         }
     }
+public int PollInput (int answer){
+        Scanner myInput = new Scanner (System.in);
+        answer = myInput.nextInt();
+        return answer;
+    } //설문자 답을 던진다??
 
 }
